c914a65bd086857286eb940aa45a9b218157dab9
gabrovnikov <103604062+gabrovnikov@users.noreply.github.com>
2023-05-18 13:11:23 -0300

    Update nmcli_eduroam.sh

--- nmcli_eduroam.sh
+++ nmcli_eduroam.sh
@@ -4,8 +4,8 @@ nmcli con add \
   ifname 'wlo1'\
   ssid 'eduroam'\
   wifi-sec.key-mgmt 'wpa-eap'\
-  802-1x.identity 'gabrvalete2904'\
-  802-1x.password 'gabr6969+'\
+  802-1x.identity '<username>'\
+  802-1x.password '<password>'\
   802-1x.system-ca-certs 'no'\
   802-1x.domain-suffix-match 'rsf.ufmg.br'\
   802-1x.eap 'peap'\
